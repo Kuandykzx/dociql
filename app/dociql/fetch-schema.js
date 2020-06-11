b0a029a1a20d543daf9d491a0d24042d398773a7
Kuandyk <kuandykzx@gmail.com>
2020-06-11 22:33:18 +0800

    request add basic auth

--- app/dociql/fetch-schema.js
+++ app/dociql/fetch-schema.js
@@ -10,10 +10,13 @@ module.exports = function (graphUrl) {
     };
 
     const responseBody = request("POST", graphUrl, {
+        'headers': {
+            'authorization': 'Basic ' + Buffer("hm_dev_92c234926daf527846999a15d4142f50b0810ac5d14a52520d8fd7988ccbb748" + ':' + '').toString('base64')
+        },
         json: requestBody
     }).getBody('utf8');
 
-    const introspectionResponse = JSON.parse(responseBody);    
+    const introspectionResponse = JSON.parse(responseBody);
 
     const jsonSchema = converter.fromIntrospectionQuery(introspectionResponse.data);
     const graphQLSchema = graphql.buildClientSchema(introspectionResponse.data, { assumeValid: true});
